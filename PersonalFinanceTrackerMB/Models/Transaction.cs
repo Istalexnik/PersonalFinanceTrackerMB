369f914e2836b0086d707b2ea529c3b0863ea0c1
Aleksandr Istomin <istalexnik@gmail.com>
2024-10-27 23:20:07 -0400

    Add Back button

--- PersonalFinanceTrackerMB/Models/Transaction.cs
+++ PersonalFinanceTrackerMB/Models/Transaction.cs
@@ -4,7 +4,7 @@ public class Transaction
 {
     public int Id { get; set; }
     public decimal Amount { get; set; }
-    public string Category { get; set; }
+    public string Category { get; set; } = string.Empty;
     public DateTime Date { get; set; }
-    public string Type { get; set; } // "Income" or "Expense"
+    public string Type { get; set; } = string.Empty;
 }
